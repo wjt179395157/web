b210bceabb7cf3acd49337ff42bd9e4dc8287c77
wjt179395157 <1793075157@qq.com>
2024-08-01 20:58:18 +0800

    增加build指令

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // 指定输出目录
+    assetsDir: 'assets', // 静态资源存放目录
+    sourcemap: false, // 是否生成 source map 文件
+    minify: 'esbuild', // 压缩选项，使用 esbuild
+    rollupOptions: {
+      input: 'src/main.js', // 指定入口文件
+      output: {
+        format: 'esm', // 输出格式
+      },
+    },
+  },
 
 })
